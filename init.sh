9ebdb7e2750d0a50bec13bbe4720f0ce50b5e23f
hyperwang <hyperwangee@gmail.com>
2017-03-16 06:03:03 -0400

    split to 2 scripts

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ if ! grep -q 'haobtc ALL=(ALL) NOPASSWD: ALL' /etc/sudoers ; then
   echo 'haobtc ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
 fi
 
-read -p "Want to disable root ssh login?[y/N]" -n 1
+read -p "Want to disable root ssh login?[y/N]"
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
@@ -24,17 +24,3 @@ rm /etc/apt/apt.conf
 sed -i 's/mirror1\.mos/mirrors\.aliyun\.com/g' /etc/apt/sources.list
 apt-get update
 apt-get install git curl zsh
-
-
-curl -sSL https://get.daocloud.io/docker | sh
-wget https://get.daocloud.io/docker/compose/releases/download/1.9.0/docker-compose-`uname -s`-`uname -m`  -O /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-docker login --username=gonbo@aliyun.com registry.cn-hangzhou.aliyuncs.com
-
-su - haobtc 
-cd ~
-echo "set ts=4
-set expandtab
-" > /home/haobtc/.vimrc
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-echo "alias 'hn=hostname'" >> /home/haobtc/.zshrc
